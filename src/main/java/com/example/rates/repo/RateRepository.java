8f9e54348f6bdef608216b4a943fa6ebabc44000
tifuseini <tifuseini@st.ug.edu.gh>
2022-06-01 21:47:57 +0000

    Repo

--- src/main/java/com/example/rates/repo/RateRepository.java
+++ src/main/java/com/example/rates/repo/RateRepository.java
@@ -7,6 +7,7 @@ import java.util.Date;
 import java.util.List;
 
 public interface RateRepository extends JpaRepository<Rate,String> {
+
     List<Rate> findByDate(Date date);
 
     Rate findByDateAndCode(Date date,String code);
