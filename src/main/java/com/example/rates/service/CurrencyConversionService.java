96cfe9ab769471b58ed95b0f8ce35aec78f44b82
tifuseini <tifuseini@st.ug.edu.gh>
2022-06-04 18:29:19 +0000

    Currency Conversoion Serive

--- src/main/java/com/example/rates/service/CurrencyConversionService.java
+++ src/main/java/com/example/rates/service/CurrencyConversionService.java
@@ -38,6 +38,7 @@ public class CurrencyConversionService {
     public Rate[] calculateByCode(@ToUpper String code,Date date) throws Exception{
         List<Rate> rates rateRepository.findByDate(date);
         if(code.equals(CurrencyExchange.BASE_CODE))
+            return rates.toArray(new Rate[0]);
 
     }
 
