e5a1ced017aedba3eeef2c73c762cad2691b3b1f
tifuseini <tifuseini@st.ug.edu.gh>
2022-06-04 18:57:46 +0000

    Rest Controller

--- src/main/java/com/example/rates/controller/CurrencyController.java
+++ src/main/java/com/example/rates/controller/CurrencyController.java
@@ -1,10 +1,12 @@
 package com.example.rates.controller;
 
 import com.example.rates.domain.CurrencyConversion;
+import com.example.rates.domain.CurrencyExchange;
 import com.example.rates.service.CurrencyConversionService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,7 +20,7 @@ public class CurrencyController {
     CurrencyConversionService conversionService;
 
     @RequestMapping("/lastest")
-    public
+    public ResponseEntity<CurrencyExchange>
 
 
 }
