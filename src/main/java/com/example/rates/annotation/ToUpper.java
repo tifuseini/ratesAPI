3d7238af891d4eb01c238a8f8c84e8e29e99ac7d
tifuseini <tifuseini@st.ug.edu.gh>
2022-06-04 16:49:25 +0000

    Annotation

--- src/main/java/com/example/rates/annotation/ToUpper.java
+++ src/main/java/com/example/rates/annotation/ToUpper.java
@@ -1,2 +1,9 @@
-package com.example.rates.annotation;public interface ToUpper {
+package com.example.rates.annotation;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Taeget
+public interface ToUpper {
 }
--- src/main/java/com/example/rates/service/CurrencyConversionService.java
+++ src/main/java/com/example/rates/service/CurrencyConversionService.java
@@ -11,7 +11,7 @@ public class CurrencyConversionService {
     @Autowired
     RateRepository rateRepository;
 
-    public CurrencyConversion convertFromTo
+    public CurrencyConversion convertFromTo(@ToUpper)
 
 
 }
