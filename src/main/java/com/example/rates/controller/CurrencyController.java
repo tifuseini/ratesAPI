f31d046b49b1cf17991bf34939da5041079f3a63
tifuseini <tifuseini@st.ug.edu.gh>
2022-06-04 18:47:20 +0000

    Rest Controller

--- src/main/java/com/example/rates/controller/CurrencyController.java
+++ src/main/java/com/example/rates/controller/CurrencyController.java
@@ -1,7 +1,9 @@
 package com.example.rates.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/currency")
 public class CurrencyController {
 }
