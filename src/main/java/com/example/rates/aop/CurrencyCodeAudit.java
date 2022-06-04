0b7b1005fb0e157910532289b4a3578ff62e46b0
tifuseini <tifuseini@st.ug.edu.gh>
2022-06-04 19:29:03 +0000

    Data

--- src/main/java/com/example/rates/aop/CurrencyCodeAudit.java
+++ src/main/java/com/example/rates/aop/CurrencyCodeAudit.java
@@ -5,6 +5,7 @@ import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.stereotype.Component;
 
 import java.lang.reflect.Parameter;
