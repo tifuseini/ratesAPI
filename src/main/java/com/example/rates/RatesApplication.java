efafe7e0bdaf354bdc4f62e82889535c08760b10
tifuseini <tifuseini@st.ug.edu.gh>
2022-06-04 19:17:08 +0000

    Data

--- src/main/java/com/example/rates/RatesApplication.java
+++ src/main/java/com/example/rates/RatesApplication.java
@@ -1,5 +1,6 @@
 package com.example.rates;
 
+import com.example.rates.repo.RateRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,7 +14,14 @@ public class RatesApplication {
 	}
 
 	@Bean
-	public CommandLineRunner
+	public CommandLineRunner data(RateRepository repository) {
+		return (args) -> {
+			repository.save(new Rate("EUR",0.88857F,new Date()));
+			repository.save(new Rate("JPY",102.17F,new Date()));
+			repository.save(new Rate("MXN",19.232F,new Date()));
+			repository.save(new Rate("GBP",0.75705F,new Date()));
+		};
+	}
 
 
 }
