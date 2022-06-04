4203b4e6a778ff117a28c21b92873b90b0c1bd4f
tifuseini <tifuseini@st.ug.edu.gh>
2022-06-04 18:33:08 +0000

    Currency Conversoion Serive

--- src/main/java/com/example/rates/service/CurrencyConversionService.java
+++ src/main/java/com/example/rates/service/CurrencyConversionService.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.Date;
 import java.util.List;
+import java.util.stream.Stream;
 
 @Service
 public class CurrencyConversionService {
@@ -46,7 +47,7 @@ public class CurrencyConversionService {
 
         if(null == baseRate) throw new Exception("Bad Base Code");
 
-        return
+        return Stream.concat(rates.stream())
 
     }
 
