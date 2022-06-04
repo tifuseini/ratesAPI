3155b7ac163c9199cc4d3ae86b505b97139e2094
tifuseini <tifuseini@st.ug.edu.gh>
2022-06-04 18:25:08 +0000

    Currency Conversoion Serive

--- src/main/java/com/example/rates/domain/CurrencyConversion.java
+++ src/main/java/com/example/rates/domain/CurrencyConversion.java
@@ -1,8 +1,12 @@
 package com.example.rates.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class CurrencyConversion {
     
     private String base;
