d1d732b721e7cf5bfa2aa6a9c27a18f4ed229ae8
tifuseini <tifuseini@st.ug.edu.gh>
2022-06-04 19:12:43 +0000

    Rest Controller

--- src/main/java/com/example/rates/controller/CurrencyController.java
+++ src/main/java/com/example/rates/controller/CurrencyController.java
@@ -36,9 +36,16 @@ public class CurrencyController {
 
     @RequestMapping("/{date}")
     public ResponseEntity<CurrencyExchange> getByDate(@PathVariable("date") @DateTimeFormat(pattern="yyyy-MM-dd") Date date, @RequestParam(name="base",defaultValue=CurrencyExchange.BASE_CODE)String base) throws Exception{
+        log.info("Number 2");
         return new ResponseEntity<CurrencyExchange>(new CurrencyExchange(base,
                 new SimpleDateFormat("yyyy-MM-dd").format(date),
                 conversionService.calculateByCode(base,date)),HttpStatus.OK);
+    }
+
+    @RequestMapping("/{amount}/{base}/to/{code}")
+    public ResponseEntity<CurrencyConversion> conversion(@PathVariable("amount")Float amount,@PathVariable("base")String base,@PathVariable("code")String code) throws Exception{
+        CurrencyConversion conversionResult = service.convertFromTo(base, code, amount);
+        return new ResponseEntity<CurrencyConversion>(conversionResult,HttpStatus.OK);
     }
 
 
