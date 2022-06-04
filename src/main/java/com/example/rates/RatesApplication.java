a38001c35908c7805aa3543d9ce36c7dea3c210f
tifuseini <tifuseini@st.ug.edu.gh>
2022-06-04 19:16:39 +0000

    Data

--- src/main/java/com/example/rates/RatesApplication.java
+++ src/main/java/com/example/rates/RatesApplication.java
@@ -1,5 +1,6 @@
 package com.example.rates;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -12,6 +13,7 @@ public class RatesApplication {
 	}
 
 	@Bean
+	public CommandLineRunner
 
 
 }
