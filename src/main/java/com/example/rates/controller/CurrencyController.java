14c0985370b615b2462a02e182964899d5f79e26
tifuseini <tifuseini@st.ug.edu.gh>
2022-06-04 18:50:41 +0000

    Rest Controller

--- src/main/java/com/example/rates/controller/CurrencyController.java
+++ src/main/java/com/example/rates/controller/CurrencyController.java
@@ -1,6 +1,7 @@
 package com.example.rates.controller;
 
 import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,5 +9,5 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/currency")
 public class CurrencyController {
 
-    private static final Logger log
+    private static final Logger log = LoggerFactory.g
 }
