f18dbbd1cf2a056eff9ccb4769efef744cf49468
tifuseini <tifuseini@st.ug.edu.gh>
2022-06-04 19:16:14 +0000

    Data

--- src/main/java/com/example/rates/RatesApplication.java
+++ src/main/java/com/example/rates/RatesApplication.java
@@ -2,6 +2,7 @@ package com.example.rates;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class RatesApplication {
@@ -9,6 +10,8 @@ public class RatesApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RatesApplication.class, args);
 	}
+
+	@Bean
 
 
 }
