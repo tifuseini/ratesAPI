a107c85291bc75d2e3cde6fc9ce677b620c39420
tifuseini <tifuseini@st.ug.edu.gh>
2022-06-04 19:08:55 +0000

    Rest Controller

--- src/main/java/com/example/rates/controller/CurrencyController.java
+++ src/main/java/com/example/rates/controller/CurrencyController.java
@@ -6,12 +6,14 @@ import com.example.rates.service.CurrencyConversionService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @RestController
 @RequestMapping("/currency")
@@ -26,7 +28,10 @@ public class CurrencyController {
     public ResponseEntity<CurrencyExchange> getLastest(@RequestParam(name = "base",
             defaultValue = CurrencyExchange.BASE_CODE)String base) throws  Exception{
 
-        return new ResponseEntity<CurrencyExchange>(new CurrencyExchange(base,new SimpleDateFormat()))
+        return new ResponseEntity<CurrencyExchange>(new CurrencyExchange(base,
+                new SimpleDateFormat("yyyy-MM-dd").format(new Date()),
+                conversionService.calculateByCode(base, new Date()), HttpStatus.OK);
+                log.info("Number 1");
 
     }
 
